2a7c64a959779a7a45dfb6a4b6fa9d2c15a79733
karthikya <karthikeya.manas@gmail.com>
2021-05-19 10:38:14 +0530

    add keys

--- website/keys/key.py
+++ website/keys/key.py
@@ -1,6 +1,53 @@
 from cryptography.fernet import Fernet
 
-def genwrite_key():
-    key = Fernet.generate_key()
-    print(key)
-print(genwrite_key())
+class Encryptor():
+
+    def key_create(self):
+        key = Fernet.generate_key()
+        return key
+
+    def key_write(self, key, key_name):
+        with open(key_name, 'wb') as mykey:
+            mykey.write(key)
+
+    def key_load(self, key_name):
+        with open(key_name, 'rb') as mykey:
+            key = mykey.read()
+        return key
+
+
+    def file_encrypt(self, key, original_file, encrypted_file):
+        
+        f = Fernet(key)
+
+        with open(original_file, 'rb') as file:
+            original = file.read()
+
+        encrypted = f.encrypt(original)
+
+        with open (encrypted_file, 'wb') as file:
+            file.write(encrypted)
+
+    def file_decrypt(self, key, encrypted_file, decrypted_file):
+        
+        f = Fernet(key)
+
+        with open(encrypted_file, 'rb') as file:
+            encrypted = file.read()
+
+        decrypted = f.decrypt(encrypted)
+
+        with open(decrypted_file, 'wb') as file:
+            file.write(decrypted)
+
+
+
+encryptor=Encryptor()
+
+mykey=encryptor.key_create()
+
+encryptor.key_write(mykey, 'key.key')
+
+loaded_key=encryptor.key_load('key.key')
+
+print(loaded_key)
