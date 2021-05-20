9e20b7ab648a875d43504e204ce8cc280ed0acf5
karthikya <karthikeya.manas@gmail.com>
2021-05-20 15:57:27 +0530

    changes made

--- website/app.py
+++ website/app.py
@@ -158,6 +158,11 @@ def account():
         flash("Please Login to view the Account details")
         return redirect(url_for("main_register"))
 
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
 
 if __name__ == "__main__":
     app.run(debug=True, port=1000, host='0.0.0.0')
+
