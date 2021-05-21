22788b179b61d8f2b0125996c08f9e3e3929987f
karthikya <karthikeya.manas@gmail.com>
2021-05-21 16:32:47 +0530

    changes made

---
+++ website/Practice/python/BinarysearchTree.py
@@ -0,0 +1,52 @@
+class Node(object):
+    def __init__(self, val):
+        self.l_child = None
+        self.r_child = None
+        self.val = val
+
+
+class BinarySearchTree(object):
+    def insert(self, root, node):
+        if root is None:
+            return node
+        if root.val < node.val:
+            root.l_child = self.insert(root.r_child, node)
+
+    def in_order_place(self, root):
+        if not root:
+            return None
+        else:
+            self.in_order_place(root.l_child)
+            print(root.val)
+            self.in_order_place(root.r_child)
+
+    def pre_order_place(self, root):
+        if not root:
+            return None
+        else:
+            print(root.val)
+            self.in_order_place(root.l_child)
+            self.in_order_place(root.r_child)
+
+    def post_order_place(self, root):
+        if not root:
+            return None
+        else:
+            self.in_order_place(root.l_child)
+            self.in_order_place(root.r_child)
+            print(root.val)
+
+
+r = Node(3)
+node = BinarySearchTree()
+nodeList = [1, 8, 5, 12, 14, 6, 15, 7, 16, 8]
+
+for nd in nodeList:
+    node.insert(r, Node(nd))
+
+print("-----In order-----")
+print(node.in_order_place(r))
+print("-----pre order-----")
+print(node.pre_order_place(r))
+print("-----post order-----")
+print(node.post_order_place(r))
---
+++ website/Practice/python/LCM.py
@@ -0,0 +1,2 @@
+num1 = int(input("please enter value: "))
+num2 = int(input("please enter second value value: "))
