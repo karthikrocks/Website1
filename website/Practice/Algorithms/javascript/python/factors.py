4723c335d58172ede7ba39c158cb8f1600c385c0
karthikya <karthikeya.manas@gmail.com>
2021-05-21 16:31:12 +0530

    changes made

--- website/Practice/Algorithms/javascript/python/factors.py
+++
@@ -1,9 +0,0 @@
-val1 = 244
-loop_num = 1
-end_num = int(val1 / 2)
-factors = []
-for i in range(loop_num, end_num):
-    if val1 % i == 0:
-        factors.append(i)
-factors.append(val1)
-print(factors)
