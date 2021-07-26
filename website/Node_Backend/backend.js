fa87e5fe2c429a8a4784189870ff6c40831f90cb
Karthikeya <karthikeya.manas@gmail.com>
2021-07-26 14:53:04 +0530

    NODE BACKEND->ADDED->CONNECTED TO FLASK

---
+++ website/Node_Backend/backend.js
@@ -0,0 +1,19 @@
+
+const express = require('express')
+const request = require('request');
+
+app = express();
+const PORT = 3000;
+
+app.get('/home', function(req, res) {
+    request('http://127.0.0.1:5000/flask', function (error, response, body) {
+        console.error('error:', error); // Print the error
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the data received
+        res.send(body); //Display the response on the website
+      });
+});
+
+app.listen(PORT, function (){
+    console.log('Listening on Port 3000');
+});
